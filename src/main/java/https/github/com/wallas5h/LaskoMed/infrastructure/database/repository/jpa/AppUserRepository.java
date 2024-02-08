5c6a6f670a75b95ecce3020a8f518aaba0e6d2e2
wallas5h@gmail.com <wallas5h@gmail.com>
2024-02-08 21:26:41 +0100

    rename entity name

--- src/main/java/https/github/com/wallas5h/LaskoMed/infrastructure/database/repository/jpa/AppUserRepository.java
+++ src/main/java/https/github/com/wallas5h/LaskoMed/infrastructure/database/repository/jpa/AppUserRepository.java
@@ -1,11 +1,14 @@
 package https.github.com.wallas5h.LaskoMed.infrastructure.database.repository.jpa;
 
 
-import https.github.com.wallas5h.LaskoMed.infrastructure.database.entity.AppUserEntity;
+import https.github.com.wallas5h.LaskoMed.infrastructure.database.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AppUserRepository extends JpaRepository<AppUserEntity, Long> {
+import java.util.Optional;
 
+@Repository
+public interface AppUserRepository extends JpaRepository<UserEntity, Long> {
+
+  Optional<UserEntity> findByUsernameOrEmail(String user, String email);
 }
