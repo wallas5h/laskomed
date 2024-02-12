0575f2f513b87dc0e9baded1e3e8f17a777b13cc
wallas5h@gmail.com <wallas5h@gmail.com>
2024-02-12 19:12:00 +0100

    add query find by doctor id

--- src/main/java/https/github/com/wallas5h/LaskoMed/infrastructure/database/repository/jpa/DoctorRepository.java
+++ src/main/java/https/github/com/wallas5h/LaskoMed/infrastructure/database/repository/jpa/DoctorRepository.java
@@ -4,7 +4,10 @@ import https.github.com.wallas5h.LaskoMed.infrastructure.database.entity.DoctorE
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<DoctorEntity, Long> {
 
+  Optional<DoctorEntity> findAllByDoctorId(Long doctorId);
 }
