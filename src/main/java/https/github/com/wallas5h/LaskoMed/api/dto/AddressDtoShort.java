b81c4ce8485f4e3814376c77350f4d7b3fb18169
wallas5h@gmail.com <wallas5h@gmail.com>
2024-02-20 17:22:25 +0100

    add shorter address dto

---
+++ src/main/java/https/github/com/wallas5h/LaskoMed/api/dto/AddressDtoShort.java
@@ -0,0 +1,20 @@
+package https.github.com.wallas5h.LaskoMed.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDtoShort {
+
+    private Long addressId;
+    private String city;
+    private String street;
+    private String houseNumber;
+    private String apartmentNumber;
+
+}
