8f7b623857a24884c9fc2678d250bb0d0e928298
wallas5h@gmail.com <wallas5h@gmail.com>
2024-02-20 17:24:45 +0100

    add patient controller and Patient service

--- src/main/java/https/github/com/wallas5h/LaskoMed/api/controller/PatientController.java
+++ src/main/java/https/github/com/wallas5h/LaskoMed/api/controller/PatientController.java
@@ -1,29 +1,95 @@
 package https.github.com.wallas5h.LaskoMed.api.controller;
 
-import https.github.com.wallas5h.LaskoMed.api.dto.PatientsDTO;
-import https.github.com.wallas5h.LaskoMed.api.mapper.PatientMapper;
-import https.github.com.wallas5h.LaskoMed.infrastructure.database.repository.jpa.AddressRepository;
-import https.github.com.wallas5h.LaskoMed.infrastructure.database.repository.jpa.PatientRepository;
+import https.github.com.wallas5h.LaskoMed.api.dto.*;
+import https.github.com.wallas5h.LaskoMed.business.services.PatientService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(PatientController.PATIENTS)
 @AllArgsConstructor
 public class PatientController {
   public static final String PATIENTS = "/patients";
+  public static final String PATIENT_ID = "/{patientId}";
+  public static final String PATIENTS_APPOINTMENTS = "/appointments";
+  public static final String PATIENTS_APPOINTMENTS_RESERVE = "/appointments/reserve";
+  public static final String PATIENT_ID_APPOINTMENTS_UPCOMING = "/{patientId}/appointments/upcoming";
+  public static final String PATIENT_ID_APPOINTMENTS_UPCOMING_ID = "/{patientId}/appointments/upcoming/{appointmentId}";
+  public static final String PATIENT_ID_APPOINTMENTS_HISTORY = "/{patientId}/appointments/history";
+  public static final String PATIENT_ID_APPOINTMENTS_HISTORY_ID = "/{patientId}/appointments/history/{appointmentId}";
 
-  private PatientRepository patientRepository;
-  private AddressRepository addressRepository;
-  private PatientMapper patientMapper;
+  private PatientService patientService;
 
   @GetMapping
- public PatientsDTO patientsList(){
-    return PatientsDTO.of(patientRepository.findAll().stream()
-        .map(a-> patientMapper.mapFromEntityToDto(a))
-        .toList()
-    );
+  public PatientsDTO patientsList() {
+    return patientService.getPatientsList();
   }
+
+  @GetMapping(PATIENT_ID)
+  public PatientDTO getPatientDetails(
+      @PathVariable Long patientId
+  ) {
+    return patientService.getPatientDetails(patientId);
+//    @TODO dodać sprawdzenie czy user wpisuje swoje patientId
+  }
+
+
+  @GetMapping(PATIENT_ID_APPOINTMENTS_UPCOMING)
+  public List<BookingAppointmentDTO> getUpcomingPatientAppointments(
+      @PathVariable Long patientId
+  ) {
+    return patientService.getPatientUpcomingAppointments(patientId);
+  }
+
+//  GET /api/patients/appointments?date=2024-02-17&specialization=cardiology&location=clinicA
+
+  @GetMapping(PATIENTS_APPOINTMENTS)
+  public List<AvailableAppointmentDTO> getAvailableMedicalAppointments(
+      @RequestParam(required = false) String specialization,
+      @RequestParam(required = false) String location) {
+
+    return patientService.getAvailableMedicalAppointments( specialization, location);
+  }
+
+  @PostMapping(
+      value = PATIENTS_APPOINTMENTS_RESERVE,
+      produces = {
+          MediaType.APPLICATION_JSON_VALUE,
+          MediaType.APPLICATION_XML_VALUE
+      }
+  )
+  public ResponseEntity<?> reserveAppointment(
+      @Valid @RequestBody AppointmentReservationRequestDto request) {
+    return patientService.reserveAppointment(request);
+  }
+
+  @PatchMapping(PATIENT_ID_APPOINTMENTS_UPCOMING)
+  public ResponseEntity<?> changeBookingsStatus(
+      @PathVariable Long patientId,
+      @Valid @RequestBody BookingAppointmentRequestDTO request
+  ) {
+    return patientService.changeBookingStatus(patientId, request);
+  }
+
+  // @TODO tu skończyłem
+  @GetMapping(PATIENT_ID_APPOINTMENTS_HISTORY)
+  public List<MedicalAppointmentDTO> getPatientAppointments(
+      @PathVariable Long patientId
+  ) {
+    return patientService.getPatientAppointments(patientId);
+  }
+
+  @GetMapping(PATIENT_ID_APPOINTMENTS_HISTORY_ID)
+  public MedicalAppointmentDTO getAppointmentDetails(
+      @PathVariable Long patientId,
+      @PathVariable Long appointmentId
+  ) {
+    return patientService.getPatientMedicalAppointmentDetails(patientId, appointmentId);
+  }
+
 }
--- src/main/java/https/github/com/wallas5h/LaskoMed/business/services/PatientService.java
+++ src/main/java/https/github/com/wallas5h/LaskoMed/business/services/PatientService.java
@@ -1,25 +1,113 @@
 package https.github.com.wallas5h.LaskoMed.business.services;
 
-import https.github.com.wallas5h.LaskoMed.api.dto.PatientsDTO;
+import https.github.com.wallas5h.LaskoMed.api.dto.*;
 import https.github.com.wallas5h.LaskoMed.api.mapper.PatientMapper;
-import https.github.com.wallas5h.LaskoMed.infrastructure.database.repository.jpa.AddressRepository;
+import https.github.com.wallas5h.LaskoMed.api.utils.EnumsContainer;
+import https.github.com.wallas5h.LaskoMed.infrastructure.database.entity.BookingAppointmentEntity;
+import https.github.com.wallas5h.LaskoMed.infrastructure.database.entity.PatientEntity;
+import https.github.com.wallas5h.LaskoMed.infrastructure.database.repository.jpa.AvailableAppointmentRepository;
 import https.github.com.wallas5h.LaskoMed.infrastructure.database.repository.jpa.PatientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class PatientService {
   private PatientRepository patientRepository;
-  private AddressRepository addressRepository;
+  private AvailableAppointmentRepository availableAppointmentRepository;
+
+  private AppointmentsService appointmentsService;
   private PatientMapper patientMapper;
 
-  public PatientsDTO getPatientsList(){
+  public PatientsDTO getPatientsList() {
     return PatientsDTO.of(patientRepository.findAll().stream()
-        .map(a-> patientMapper.mapFromEntityToDto(a))
+        .map(a -> patientMapper.mapFromEntityToDto(a))
         .toList()
     );
+  }
+
+  public PatientDTO getPatientDetails(Long patientId) {
+    Optional<PatientEntity> entity = patientRepository.findById(patientId);
+
+    return entity.map(patientEntity -> patientMapper.mapFromEntityToDto(patientEntity))
+        .orElseThrow(() -> new EntityNotFoundException(
+            "PatientEntity not found, patientId: [%s]".formatted(patientId)
+        ));
+
+  }
+
+  public List<BookingAppointmentDTO> getPatientUpcomingAppointments(Long patientId) {
+    return appointmentsService.getPatientUpcomingBookings(patientId);
+  }
+
+  public ResponseEntity<?> changeBookingStatus(Long patientId, BookingAppointmentRequestDTO request) {
+    int changedBookingStatus = appointmentsService.changeBookingStatus(patientId, request);
+    if (changedBookingStatus < 1) {
+      return ResponseEntity.status(400).body("Incorrect input data");
+    } else {
+      if (request.getBookingStatus().toUpperCase().equals(EnumsContainer.BookingStatus.CANCELLED.name())){
+        return ResponseEntity.ok("Booking canceled successfully");
+      }
+      return ResponseEntity.ok("Booking changed successfully");
+    }
+  }
+
+  public List<MedicalAppointmentDTO> getPatientAppointments(Long patientId) {
+    return appointmentsService.getPatientMedicalAppointments(patientId);
+  }
+
+  public boolean isBookingStatusValid(BookingAppointmentRequestDTO request) {
+    try {
+      EnumsContainer.BookingStatus status = EnumsContainer.BookingStatus.valueOf(request.getBookingStatus().toUpperCase());
+      return true;
+    } catch (IllegalArgumentException e) {
+      return false;
+    }
+  }
+
+  public MedicalAppointmentDTO getPatientMedicalAppointmentDetails(Long patientId, Long appointmentId) {
+    return appointmentsService.getPatientMedicalAppointmentDeatails(appointmentId);
+  }
+
+  public List<AvailableAppointmentDTO> getAvailableMedicalAppointments(String specialization, String location) {
+    LocalDate date= LocalDate.now();
+    specialization = specialization == null ? "" : specialization;
+    location = location == null ? "" : location;
+
+    return appointmentsService.getAvailableMedicalAppointments(date, specialization, location);
+  }
+
+  public ResponseEntity<?> reserveAppointment(AppointmentReservationRequestDto request) {
+    if (validatePatientDateTimeCollisionAppointments(request)){
+      return ResponseEntity.status(400).body("Patient already has an appointment at this time.");
+    };
+
+    BookingAppointmentEntity newBookingAppointment = appointmentsService.reserveAppointment(request);
+    if (newBookingAppointment==null){
+      return ResponseEntity.status(404).body(
+          "The appointment is no longer available. The appointment has been reserved by another patient.");
+    }
+    if (newBookingAppointment.getBookingId() != null) {
+      return ResponseEntity.ok("Booking appointment successfully");
+    } else {
+      return ResponseEntity.status(500).body("Sorry try later");
+    }
+  }
+
+  private boolean validatePatientDateTimeCollisionAppointments(AppointmentReservationRequestDto request) {
+    boolean isCollision=false;
+    Optional<BookingAppointmentEntity> appointmentByDateAndPatient = appointmentsService.findAppointmentByDateAndPatient(request);
+    if (appointmentByDateAndPatient.isPresent()){
+      isCollision=true ;
+    }
+    return isCollision;
   }
 }
