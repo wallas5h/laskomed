22c617c55c076ab45f3e765f58f3238bf9e384a6
wallas5h@gmail.com <wallas5h@gmail.com>
2024-03-02 15:06:36 +0100

    add

---
+++ src/main/resources/db/migration/V3_3__init_laskomed_insert_data.sql
@@ -0,0 +1,10 @@
+CREATE SEQUENCE app_user_seq START 2;
+
+INSERT INTO app_user (username, password, email, enabled, confirmed)
+VALUES ('john.doe', '$2a$12$qm09Fx35l7AzYQ8eWTVkru72plkZW5PUbi7lXvxILDsVpxfpXZcGW', 'john.doe@example.com', true, true);
+
+SELECT nextval('app_user_seq');
+
+INSERT INTO patient (name, surname, pesel, birthdate, email, phone, gender, medical_package, address_id, user_id)
+VALUES ('John', 'Doe', '90011578901', '1990-01-15', 'john.doe@example.com', '123456789', 'Male', 'premium', 1, currval('app_user_seq'));
+
